96ae5f08d457e399625295646768ca6b65da4730
Иван Николаев <niksyper@gmail.com>
2018-04-23 14:30:19 +0300

    3

--- фабрика на WPF/MainWindow.xaml.cs
+++ фабрика на WPF/MainWindow.xaml.cs
@@ -24,5 +24,67 @@ namespace фабрика_на_WPF
         {
             InitializeComponent();
         }
+
+        private void Войти(object sender, RoutedEventArgs e)
+        {
+            using (var context = new clothEntities())
+            {
+                var a = context.Пользователь.Where(l => l.Логин == Логин_лог.Text).Where(p => p.Пароль == Пароль_лог.Password).Select(R => R.Роль);
+                var b = context.Пользователь.Where(l => l.Логин == Логин_лог.Text).Where(p => p.Пароль == Пароль_лог.Password).FirstOrDefault();
+                int aa = a.First();
+                var c = context.Роль.Where(s => s.ID == aa).First();
+                if (Логин_лог.Text  != "" && Пароль_лог.Password !="")
+                {
+                  if (a.Count() != 0)
+                  {
+                    Имя_в_инфобордере.Text = b.Имя;
+                    Тип_пользователя_в_инфобордере.Text = c.Роль1;
+                        Кнопка_выхода.Visibility = Visibility.Visible;
+                  }
+                        else
+                        {
+                        MessageBox.Show("Не правильный логин или пароль.");
+                        }
+                }
+                else
+                {
+                    MessageBox.Show("Заполните все поля.");
+                }
+              
+            }
+
+        }
+
+        private void регистрация(object sender, RoutedEventArgs e)
+        {
+            if (имя_рег.Text != "" && пароль_рег.Password != "" && имя_рег.Text != "")
+            {
+                using (var context = new clothEntities())
+                {
+                    Пользователь регистр = new Пользователь()
+                    {
+                        Имя = имя_рег.Text,
+                        Логин = логин_рег.Text,
+                        Пароль = пароль_рег.Password,
+                        Роль = 1
+                    };
+                    context.Пользователь.Add(регистр);
+                    context.SaveChanges();
+                   // MessageBox.Show("Вы успешно зарегестрировались." + "Хотите войти?", "Вход");
+                }
+            }
+            else
+            {
+                 MessageBox.Show("Вы успешно зарегестрировались. Хотите войти?", "Вход", MessageBoxButton.YesNo);
+                MessageBox.Show("Заполните все поля.");
+                
+            }
+        }
+        private void Выйти_из_аккаунта(object sender, RoutedEventArgs e)
+        {
+            MainWindow a = new MainWindow();
+            a.Show();
+            Close();
+        }
     }
 }
